59cd6fa62eedcbfd94e677c9ecb09bf9a4c0b877
nemeth <istvan-nemeth@windowslive.com>
2019-02-27 22:26:02 +0100

    Arrange Tests First phase.
    jUnit5 version set to 5.3.2, now it runs tests
    
    remaining:
       - Mapper tests (4)
       - Unit tests
       - IT tests
          - Controller
          - Service

--- src/test/java/hu/nemethi/mybudget/MyBudgetTest.java
+++ src/test/java/hu/nemethi/mybudget/MyBudgetTest.java
@@ -1,37 +1,27 @@
 package hu.nemethi.mybudget;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public abstract class MyBudgetTest {
 
-    protected static HttpEntity<String> AUTH_HEADER;
+//    protected static HttpEntity<String> AUTH_HEADER;
 
-    @Autowired
-    protected TestRestTemplate restClient;
+    //@Autowired
+    //protected TestRestTemplate restClient;
 
-    @Autowired
-    protected JdbcTemplate jdbcTemplate;
-
-    @LocalServerPort
-    protected int port;
-
-    protected URL baseApiUrl;
-
-    @BeforeEach
-    public void setupAPIUrl() {
-        try {
-            this.baseApiUrl = new URL("http://localhost:" + port);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
+//    @Autowired
+//    protected JdbcTemplate jdbcTemplate;
+//
+//    @LocalServerPort
+//    protected int port;
+//
+//    protected URL baseApiUrl;
+//
+//    @BeforeEach
+//    public void setupAPIUrl() {
+//        try {
+//            this.baseApiUrl = new URL("http://localhost:" + port);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
